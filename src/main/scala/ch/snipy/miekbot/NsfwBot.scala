13215baeda4b329bf3bbdb1f708421d28c7d971e
Sylvain Julmy <sylvain.julmy@gmail.com>
2018-03-12 16:46:22 +0100

    Implements first NSFW command but use another subreddit than /ass for the moment

--- src/main/scala/ch/snipy/miekbot/NsfwBot.scala
+++ src/main/scala/ch/snipy/miekbot/NsfwBot.scala
@@ -1,14 +1,59 @@
 package ch.snipy.miekbot
 
+import java.util
+import java.util.Collections
+import java.util.stream.Collectors
+
 import info.mukel.telegrambot4s.api.declarative.Commands
-import info.mukel.telegrambot4s.api.{Polling, TelegramBot}
+import info.mukel.telegrambot4s.api.TelegramBot
+import info.mukel.telegrambot4s.methods.{ChatAction, SendChatAction, SendPhoto, SendSticker}
+import info.mukel.telegrambot4s.models.InputFile
+import net.dean.jraw.models.{Submission, SubredditSort, TimePeriod}
 
-trait NsfwBot extends TelegramBot with Commands with Polling with HttpRequest with RedditApi {
+import scala.util.{Failure, Random, Success}
 
-  println(redditToken)
+abstract class NsfwBot extends TelegramBot with Commands with RedditApi with Requests {
 
-  onCommand("/ass") { msg =>
+  private val LIMIT = 100
 
+  private lazy val assPosts: util.List[Submission] = reddit.subreddit("EarthPorn").posts()
+    .sorting(SubredditSort.TOP)
+    .limit(LIMIT)
+    .timePeriod(TimePeriod.ALL)
+    .build()
+    .next().stream().collect(Collectors.toList[Submission])
+
+  private def buildImageUrl(sub: Submission): String = {
+    if (sub.getUrl.contains("imgur.com"))
+      if (sub.getUrl.contains("jpg"))
+        s"https://i.imgur.com/${sub.getUrl.split("/").last}"
+      else
+        s"https://i.imgur.com/${sub.getUrl.split("/").last}.jpg"
+    else
+      sub.getUrl
+  }
+
+  onCommand("/ass") { implicit msg =>
+
+    val sub = assPosts.get(Random.nextInt(LIMIT))
+    val url = buildImageUrl(sub)
+
+    println(url)
+
+    requestImage(url).onComplete {
+      case Failure(exception) =>
+        reply {
+          s"""
+             |error :
+             |${exception.getMessage}
+             |${exception.getCause}
+          """.stripMargin
+        }
+      case Success(byteString) =>
+        val photo = InputFile(sub.getFullName + ".jpg", byteString)
+        request(SendChatAction(msg.source, ChatAction.UploadPhoto))
+        request(SendPhoto(msg.source, photo))
+    }
   }
 
   onCommand("/boobs") { msg =>
@@ -53,21 +98,4 @@ trait NsfwBot extends TelegramBot with Commands with Polling with HttpRequest wi
   onCommand("/sylvainsepougne") { msg =>
 
   }
-
-  onCommand("/") { msg =>
-
-  }
-
-  onCommand("/ass") { msg =>
-
-  }
-
-  onCommand("/ass") { msg =>
-
-  }
-
-  onCommand("/ass") { msg =>
-
-  }
-
 }
