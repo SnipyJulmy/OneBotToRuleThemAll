9d51aa3c502dbddc000924c28b26fce2144a3e21
Sylvain Julmy <sylvain.julmy@gmail.com>
2018-03-13 17:40:19 +0100

    Refactor some code

--- src/main/scala/ch/snipy/miekbot/NsfwBot.scala
+++ src/main/scala/ch/snipy/miekbot/NsfwBot.scala
@@ -1,44 +1,23 @@
 package ch.snipy.miekbot
 
-import java.util
-import java.util.Collections
-import java.util.stream.Collectors
-
-import info.mukel.telegrambot4s.api.declarative.Commands
+import ch.snipy.miekbot.Debug._
 import info.mukel.telegrambot4s.api.TelegramBot
-import info.mukel.telegrambot4s.methods.{ChatAction, SendChatAction, SendPhoto, SendSticker}
-import info.mukel.telegrambot4s.models.InputFile
-import net.dean.jraw.models.{Submission, SubredditSort, TimePeriod}
+import info.mukel.telegrambot4s.api.declarative.Commands
+import info.mukel.telegrambot4s.methods._
+import info.mukel.telegrambot4s.models.{InputFile, Message}
+import net.dean.jraw.models.Submission
 
 import scala.util.{Failure, Random, Success}
 
-abstract class NsfwBot extends TelegramBot with Commands with RedditApi with Requests {
+trait NsfwBot extends TelegramBot with Commands with RedditApi with Requests {
 
-  private val LIMIT = 100
+  lazy val assPosts: Vector[Submission] = posts("EarthPorn")
+  lazy val boobPosts: Vector[Submission] = posts("spaceporn")
 
-  private lazy val assPosts: util.List[Submission] = reddit.subreddit("EarthPorn").posts()
-    .sorting(SubredditSort.TOP)
-    .limit(LIMIT)
-    .timePeriod(TimePeriod.ALL)
-    .build()
-    .next().stream().collect(Collectors.toList[Submission])
 
-  private def buildImageUrl(sub: Submission): String = {
-    if (sub.getUrl.contains("imgur.com"))
-      if (sub.getUrl.contains("jpg"))
-        s"https://i.imgur.com/${sub.getUrl.split("/").last}"
-      else
-        s"https://i.imgur.com/${sub.getUrl.split("/").last}.jpg"
-    else
-      sub.getUrl
-  }
+  def sendImage(url: String, imageName: String = "default")(implicit msg: Message): Unit = {
 
-  onCommand("/ass") { implicit msg =>
-
-    val sub = assPosts.get(Random.nextInt(LIMIT))
-    val url = buildImageUrl(sub)
-
-    println(url)
+    debug(url)
 
     requestImage(url).onComplete {
       case Failure(exception) =>
@@ -50,52 +29,87 @@ abstract class NsfwBot extends TelegramBot with Commands with RedditApi with Req
           """.stripMargin
         }
       case Success(byteString) =>
-        val photo = InputFile(sub.getFullName + ".jpg", byteString)
+        val photo = InputFile(imageName + ".jpg", byteString)
         request(SendChatAction(msg.source, ChatAction.UploadPhoto))
         request(SendPhoto(msg.source, photo))
     }
   }
 
-  onCommand("/boobs") { msg =>
+  def buildImageUrl(sub: Submission): String = {
+    if (sub.getUrl.contains("imgur.com"))
+      if (sub.getUrl.contains("jpg"))
+        s"https://i.imgur.com/${sub.getUrl.split("/").last}"
+      else
+        s"https://i.imgur.com/${sub.getUrl.split("/").last}.jpg"
+    else
+      sub.getUrl
+  }
+
+  onCommand("/ass") { implicit msg =>
+    val sub = assPosts.rndPick
+    val url = buildImageUrl(sub)
+    sendImage(url, sub.getFullName)(msg)
+  }
+
+  onCommand("/boobs") { implicit msg =>
+    val sub = boobPosts.rndPick
+    val url = buildImageUrl(sub)
+    sendImage(url, sub.getFullName)(msg)
+  }
+
+  onCommand("/nsfw") {
+    msg =>
 
   }
 
-  onCommand("/nsfw") { msg =>
+  onCommand("/auranesexcite") {
+    msg =>
 
   }
 
-  onCommand("/auranesexcite") { msg =>
+  onCommand("/remisenvoieenlair") {
+    msg =>
 
   }
 
-  onCommand("/remisenvoieenlair") { msg =>
+  onCommand("/emmasetouche") {
+    msg =>
 
   }
 
-  onCommand("/emmasetouche") { msg =>
+  onCommand("/justinsebranle") {
+    msg =>
 
   }
 
-  onCommand("/justinsebranle") { msg =>
+  onCommand("/morgansepelelejonc") {
+    msg =>
 
   }
 
-  onCommand("/morgansepelelejonc") { msg =>
+  onCommand("/thomassefrotte") {
+    msg =>
 
   }
 
-  onCommand("/thomassefrotte") { msg =>
-
-  }
-  onCommand("/jeannemouille") { msg =>
-
-  }
-
-  onCommand("/nathajute") { msg =>
+  onCommand("/jeannemouille") {
+    msg =>
 
   }
 
-  onCommand("/sylvainsepougne") { msg =>
+  onCommand("/nathajute") {
+    msg =>
 
+  }
+
+  onCommand("/sylvainsepougne") {
+    msg =>
+
+  }
+
+  implicit class RandomPick(subs: Vector[Submission]) {
+    private val itr: Int = Random.nextInt(LIMIT)
+    debug(s"rndPick : $itr, subs.length : ${subs.length}")
+    def rndPick: Submission = subs(itr)
   }
 }
